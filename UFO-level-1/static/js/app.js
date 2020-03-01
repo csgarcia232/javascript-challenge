2f4b89e03c8e231b8195c6c8432109305b906b3c
Candice Garcia <candicegarcia@Candices-MacBook-Air.local>
2020-02-29 22:44:32 -0600

    Restarting from the beginning

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,9 +3,3 @@ var tableData = data;
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
-
-// Console.log the data from data.js
-console.log(data);
-
-var submitButton = d3.select("#filter-btn-date");
-var inputField = d3.select("#datetime");
